c66783975745a802d918fc21ebf77bfbb35e6d53
Erick Romero <zyst@gmx.com>
2016-05-10 21:02:52 -0500

    Boilerplate completed

---
+++ contact-list-firebase/src/actions/AppActions.js
@@ -0,0 +1,8 @@
+import AppDispatcher from '../dispatcher/AppDispatcher';
+import AppConstants from '../constants/AppConstants';
+
+const AppActions = {
+
+};
+
+export default AppActions;
--- contact-list-firebase/src/index.js
+++ contact-list-firebase/src/index.js
@@ -1,1 +1,9 @@
-console.log('JS is working!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
