cb8af59d686171ca6936f2b80ad353023bbe1634
Erick Romero <zyst@gmx.com>
2016-05-17 23:25:00 -0500

    Base layout for my new project complete

--- stock-client/src/components/App.js
+++ stock-client/src/components/App.js
@@ -27,8 +27,26 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Haters in the club say whaa!</h1>
+      <div className="row">
+        <div className="col-md-2">
+          <div className="card">
+            <div className="card-block">
+              <p className="card-text">Left</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-8">
+          <div className="jumbotron">
+            <h1>Center</h1>
+          </div>
+        </div>
+        <div className="col-md-2">
+          <div className="card">
+            <div className="card-block">
+              <h3 className="card-text">Right</h3>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
