54e7d3e09c57b6b49df6d278965ffe6e01c65fe6
Zyst <zyst@gmx.com>
2016-12-26 21:04:51 -0600

    Import main files

--- redux-tutorial/client/reduxstagram.js
+++ redux-tutorial/client/reduxstagram.js
@@ -1,1 +1,7 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+// Import CSS
+import css from './styles/style.styl';
+
+render(<p>Hiya</p>, document.getElementById('root'));
