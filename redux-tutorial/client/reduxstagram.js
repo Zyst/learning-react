3dc2d592a021a68b9c659b640ff3a9bb36d5447d
Zyst <zyst@gmx.com>
2016-12-26 21:18:21 -0600

    Finished 4

--- redux-tutorial/client/reduxstagram.js
+++ redux-tutorial/client/reduxstagram.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 // Import CSS
 import css from './styles/style.styl';
 
 // Import Components
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main />, document.getElementById('root'));
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+);
+
+render(router, document.getElementById('root'));
