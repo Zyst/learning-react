5c237c4e15d4c69de35e029ff53f1449a5d784e1
Zyst <zyst@gmx.com>
2016-12-26 21:05:00 -0600

    Touch our main components

---
+++ redux-tutorial/client/components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Link to="/">
+          <h1>Reduxstagram</h1>
+        </Link>
+      </div>
+    );
+  }
+}
