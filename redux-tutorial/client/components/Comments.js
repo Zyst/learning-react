aad4e88aca4a4abd697827e36e75d40516d75953
Zyst <zyst@gmx.com>
2016-12-28 21:54:43 -0600

    Finished 14

--- redux-tutorial/client/components/Comments.js
+++ redux-tutorial/client/components/Comments.js
@@ -1,7 +1,27 @@
 import React from 'react'
 
-const Comments = ({}) => <div className="comment">
-  I'm the comments
-</div>
+const Comments = ({
+  postComments
+}) =>
+  <div className="comments">
+    {
+      postComments.map((comment, i) =>
+        <div className="comment" key={i}>
+          <p>
+            <strong>{comment.user}</strong>
+            {comment.text}
+            <button className="remove-comment">
+              &times;
+            </button>
+          </p>
+        </div>
+      )
+    }
+    <form ref="commentForm" className="comment-form">
+      <input type="text" ref="author" placeholder="author" />
+      <input type="text" ref="comment" placeholder="comment" />
+      <input type="submit" hidden />
+    </form>
+  </div>
 
 export default Comments
--- redux-tutorial/client/components/Single.js
+++ redux-tutorial/client/components/Single.js
@@ -8,13 +8,13 @@ class Single extends Component {
     const i = this.props.posts.findIndex(post =>
       post.code === this.props.params.postId
     )
-
     const post = this.props.posts[i]
+    const postComments = this.props.comments[this.props.params.postId] || []
 
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
-        <Comments />
+        <Comments postComments={postComments} />
       </div>
     )
   }
