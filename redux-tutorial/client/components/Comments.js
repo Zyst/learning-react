a97b02943584e987261c0dd2be8437fa13dca49d
Zyst <zyst@gmx.com>
2016-12-28 22:40:03 -0600

    Finished 15

--- redux-tutorial/client/components/Comments.js
+++ redux-tutorial/client/components/Comments.js
@@ -1,27 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Comments = ({
-  postComments
-}) =>
-  <div className="comments">
-    {
-      postComments.map((comment, i) =>
-        <div className="comment" key={i}>
-          <p>
-            <strong>{comment.user}</strong>
-            {comment.text}
-            <button className="remove-comment">
-              &times;
-            </button>
-          </p>
-        </div>
-      )
-    }
-    <form ref="commentForm" className="comment-form">
-      <input type="text" ref="author" placeholder="author" />
-      <input type="text" ref="comment" placeholder="comment" />
-      <input type="submit" hidden />
-    </form>
-  </div>
+class Comments extends Component {
+  renderComment(comment, i) {
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">
+            &times;
+        </button>
+        </p>
+      </div>
+    )
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+
+    const { postId } = this.props.params
+    const author = this.refs.author.value
+    const comment = this.refs.comment.value
+
+    this.props.addComment(postId, author, comment)
+  }
+
+  render() {
+    return (
+      <div className="comments">
+        {this.props.postComments.map(this.renderComment)}
+        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit.bind(this)}>
+          <input type="text" ref="author" placeholder="author" />
+          <input type="text" ref="comment" placeholder="comment" />
+          <input type="submit" hidden />
+        </form>
+      </div>
+    )
+  }
+}
 
 export default Comments
