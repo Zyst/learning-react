8e0c087bdc66a5e7bdbb27bea1710633ac5fddb1
Zyst <zyst@gmx.com>
2016-12-28 21:45:01 -0600

    13 done

---
+++ redux-tutorial/client/components/Comments.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Comments = ({}) => <div className="comment">
+  I'm the comments
+</div>
+
+export default Comments
--- redux-tutorial/client/components/Single.js
+++ redux-tutorial/client/components/Single.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import Photo from './Photo'
+import Comments from './Comments'
 
 class Single extends Component {
   render() {
+    const i = this.props.posts.findIndex(post =>
+      post.code === this.props.params.postId
+    )
+
+    const post = this.props.posts[i]
+
     return (
       <div className="single-photo">
-        I'm the single grid
+        <Photo i={i} post={post} {...this.props} />
+        <Comments />
       </div>
     )
   }
