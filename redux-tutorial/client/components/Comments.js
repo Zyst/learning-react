6f9bfb274ccf690036fd6423acda68a6580fc1f3
Zyst <zyst@gmx.com>
2016-12-28 22:59:34 -0600

    Finished 16

--- redux-tutorial/client/components/Comments.js
+++ redux-tutorial/client/components/Comments.js
@@ -7,9 +7,9 @@ class Comments extends Component {
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment">
+          <button className="remove-comment" onClick={this.props.removeComment.bind(null, this.props.params.postId, i)}>
             &times;
-        </button>
+          </button>
         </p>
       </div>
     )
@@ -23,12 +23,13 @@ class Comments extends Component {
     const comment = this.refs.comment.value
 
     this.props.addComment(postId, author, comment)
+    this.refs.commentForm.reset()
   }
 
   render() {
     return (
       <div className="comments">
-        {this.props.postComments.map(this.renderComment)}
+        {this.props.postComments.map(this.renderComment.bind(this))}
         <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit.bind(this)}>
           <input type="text" ref="author" placeholder="author" />
           <input type="text" ref="comment" placeholder="comment" />
